2568d781060367fe8972a5950926bfdbeb024102
H0R15H0 <shohori13@gmail.com>
2021-01-01 23:16:33 +0900

    themaを追加

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <Header/>
-    <router-view/>
+    <VApp>
+      <Header/>
+      <router-view/>
+    </VApp>
   </div>
 </template>
 
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,13 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#DAFED8", 
+        secondary: "#DAFED8", 
+        accent: "#DAFED8", 
+      },
+    },
+  },
 });
