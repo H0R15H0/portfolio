82f488d764449a6971fd993c5c148c8bd4896f04
H0R15H0 <shohori13@gmail.com>
2021-06-13 17:48:10 +0900

    aboutVueを修正。

---
+++ src/components/ProfileCards.vue
@@ -0,0 +1,37 @@
+<template>
+<VContainer>
+  <VRow justify="center"> <!-- 資格が増えてカードが3つになったら space-around に -->
+    <VCol sm="3" cols="7" v-for="item in items" :key="item.title">
+      <VCard color="primary" flat>
+        <VImg :src="item.imgSrc" width="70%" aspect-ratio="1" style="border-radius:100px; margin:auto;"></VImg>
+        <VRow justify="center">
+          <VCardTitle class="accent--text font-weight-bold">{{ item.title }}</VCardTitle>
+        </VRow>
+        <VRow justify="center">
+          <ul>
+            <li v-for="content in item.contents" :key="content">{{content}}</li>
+          </ul>
+        </VRow>
+      </VCard>
+    </VCol>
+  </VRow>
+</VContainer>
+</template>
+
+<script>
+export default { 
+  name: "ProfileCards",
+  data() {
+    return {
+      items: [
+        { title: "Profile", imgSrc: require("../assets/profile.png"), contents:["堀崎祥", "名古屋大学", "2001/01/31"] },
+        { title: "Favorite", imgSrc: require("../assets/aquarium.png"), contents:["水族館", "映画鑑賞"] },
+        // { title: "Qualification", imgSrc: require("../assets/qualification.png"), contents:["TOIEC: 000", "hogehoge"] },
+      ]
+    }
+  },
+}
+</script>
+
+<style>
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,44 +3,46 @@
     <VRow justify="center">
       <h1>About</h1>
     </VRow>
-    <VRow justify="center" align="center">
+    <!-- <VRow justify="center" align="center">
       <VCol sm="3" cols="12">
         <VImg :src="require('../assets/horisho.png')" max-width="30vw" max-height="30vh" style="margin:auto;" contain ></VImg>
       </VCol>
-      <VCol sm="3" cols="12">
-        <VList color="primary">
-          <VListItem v-for="item in items" :key="item.id" class="my-n3">
-            <VListItemTitle class="text-right">{{item.title}}:</VListItemTitle>
-            <VListItemSubtitle class="black--text text-left pl-2">{{item.content}}</VListItemSubtitle>
-          </VListItem>
-        </VList>
-      </VCol>
+    </VRow> -->
+    <VRow>
+      <ProfileCards></ProfileCards>
     </VRow>
     <VRow justify="center">
       <VCol sm="9" cols="12" class="text-left">
         名古屋大学情報学部コンピュータ科学科に所属する３年生です。<br>
         大学入学後アプリ開発サークルに所属し、それからプログラミングに触れ始めました。
         なのでプログラミング経験は２年ほどです。現在代表を勤めております。後輩たちの成長が凄まじく追い越されないよう日々勉強してます。。。<br>
-        最近はRails, Vueばかりを触っていますが、新しい技術に触れるのが好きなのでその他言語も広く浅く学んでいます。
+      </VCol>
+    </VRow>
+    <VRow justify="center">
+      <VCol sm="9" cols="12" class="text-left">
+        <VRow justify="left" class="mb">
+          <VCol sm="2" cols="3" class="text-left">
+            <h2><span class="font-weight-black accent--text mr-1">|</span>Recent</h2>
+          </VCol>
+        </VRow>
+        地獄のような学生実験が終わりました！！！
+        ずっと触りたかったGoをようやく始められます！このポートフォリオのバックエンドをGoのフレームワークGinで開発しようと考えています。
+        サマーインターンに備えて基礎的な部分(TCP/IP等)からインフラ部分(Docker, AWS, CICD)まで学んでいこうと思っています。
+        TOEICの勉強にも取り組んでいます。目標は院試の一つの基準でもある807点です。
       </VCol>
     </VRow>
   </VContainer>
 </template>
 
 <script>
+import ProfileCards from "@/components/ProfileCards.vue"
+
 export default {
-  data() {
-    return {
-      items: [
-        { id: 1, title: "Name", content: "horisho" },
-        { id: 2, title: "University", content: "Nagoya.U" },
-        { id: 3, title: "Birth", content: "2001/01/31" },
-      ]
-    }
-  }
+  components: {
+    ProfileCards,
+  },
 }
 </script>
 
-<style>
-
+<style scoped>
 </style>
