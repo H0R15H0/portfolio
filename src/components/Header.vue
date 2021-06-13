8ffde2f92477fba9c02484728842e95ff197a2c6
H0R15H0 <shohori13@gmail.com>
2021-06-13 20:49:13 +0900

    レスポンシブなヘッダーにした。

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,32 @@
 <template>
   <header>
-    <VAppBar app elevate-on-scroll color="primary">
-      <VToolbarItems>
-        <VBtn v-for="item in items" :key="item.title" :href="item.route" depressed color="primary black--text" large>
-        {{ item.title }}
-        </VBtn>
-      </VToolbarItems>
-
+    <VAppBar app flat color="primary">
+      <div v-if="$vuetify.breakpoint.xs">
+        <VAppBarNavIcon @click.stop="drawer = !drawer"></VAppBarNavIcon>
+        <VNavigationDrawer v-model="drawer" app class="primary" hide-overlay width="100vw" height="auto" style="box-shadow:initial;">
+          <VList>
+            <VListItem>
+              <VListItemIcon @click="drawer = !drawer">
+                <VIcon>mdi-close</VIcon>
+              </VListItemIcon>
+            </VListItem>
+            <div v-for="item in items" :key="item.title" >
+              <VListItem @click="$router.push(item.route).catch(err => {drawer = !drawer})">
+                <VListItemContent>
+                    <span>{{item.title}}</span>
+                </VListItemContent>
+              </VListItem>
+            </div>
+          </VList>
+        </VNavigationDrawer>
+      </div>
+      <div v-else>
+        <VToolbarItems>
+          <VBtn v-for="item in items" :key="item.title" @click="$router.push(item.route).catch(err => {})" depressed color="primary black--text" x-large>
+          {{ item.title }}
+          </VBtn>
+        </VToolbarItems>
+      </div>
     </VAppBar>
   </header>
 </template>
@@ -22,14 +42,21 @@ export default {
         { title: "Products", route: "/products"},
         // { title: "Posts", route: "/#posts"},
       ],
+      drawer: false,
+      group: null,
     }
   },
+  watch: {
+      group () {
+        this.drawer = false
+      },
+    },
   methods: {
-    moveToTop() {
-      this.$router.push({name: 'Home'}, () => {})
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
-  }
+    // moveToTop() {
+    //   this.$router.push({name: 'Home'}, () => {})
+    //   window.scrollTo({ top: 0, behavior: 'smooth' });
+    // }
+  },
 }
 </script>
 
@@ -38,5 +65,29 @@ header {
   position: fixed;
   top: 0;
   z-index: 10;
+}
+@keyframes navigation-move {
+  from {
+      transform: translateY(-100%);
+  }
+  to {
+      transform: translateY(0%);
+  }
+}
+@keyframes navigation-close {
+  from {
+      transform: translateY(0%);
+  }
+  to {
+      transform: translateY(-100%);
+  }
+}
+.v-navigation-drawer--close{
+    animation-name: navigation-close;
+    animation-duration: 0.4s;
+}
+.v-navigation-drawer--open {
+    animation-name: navigation-move;
+    animation-duration: 0.4s;
 }
 </style>
