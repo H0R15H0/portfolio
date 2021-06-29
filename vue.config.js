d0a948bf3c25ceb5586d558532d00e9cc3172164
H0R15H0 <shohori13@gmail.com>
2021-06-29 22:26:42 +0900

    title 追加

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.js', // 必須パラメータ
+      title: "Horisho's Portfolio",
+    }
+  },
   "transpileDependencies": [
     "vuetify"
   ]
