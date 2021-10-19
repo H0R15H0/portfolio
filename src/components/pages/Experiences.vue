f8ba0299d3084dd191f55e5602a7495f51743a50
H0R15H0 <shohori13@gmail.com>
2021-10-20 05:19:21 +0900

    Fix: margin

--- src/components/pages/Experiences.vue
+++ src/components/pages/Experiences.vue
@@ -4,8 +4,8 @@
       <h1>Experiences</h1>
     </VRow>
     <ExperiencesHistory></ExperiencesHistory>
-    
-    <VRow justify="center" class="mb-5">
+
+    <VRow justify="center" class="my-5">
       <h1>Conferences / LT</h1>
     </VRow>
     <Conferences></Conferences>
